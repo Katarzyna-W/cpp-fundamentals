a2745e4a089a9f4357db2183b283fdafb55be8c1
Katarzyna-W <39034151+Katarzyna-W@users.noreply.github.com>
2025-07-07 16:01:41 +0200

    Update nwdNww.hpp

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0){
+        return rhs;
+    }
+    return NWD(rhs % lhs, lhs);
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int smaller = std::min(lhs, rhs);
+    int bigger = std::max(lhs, rhs);
+    
+    for (std::size_t i = bigger; i < lhs * rhs; i += bigger){
+        if (i % smaller == 0){
+            return i;
+        }
+        
+    }
+    return lhs * rhs;
 }
