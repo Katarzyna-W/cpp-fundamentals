59bb0fdd8ec92f9ae1d78b4a8b25d9f46b4b6600
Katarzyna-W <39034151+Katarzyna-W@users.noreply.github.com>
2025-07-07 15:29:15 +0200

    Update fibonacci.hpp

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -19,12 +19,12 @@ int fibonacci_iterative(int sequence) {
     return next;
 }
 
-
 int fibonacci_recursive(int sequence) {
     if (sequence == 1 or sequence == 2) {
         return 1;
     } else if (sequence == 0) {
         return 0;
     }
-    return fibonacci_recursive(sequence - 2) + fibonacci_recursive(sequence - 1); 
+    return fibonacci_recursive(sequence - 2) +
+           fibonacci_recursive(sequence - 1);
 }
