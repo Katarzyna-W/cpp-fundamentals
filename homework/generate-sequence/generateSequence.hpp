2c432a78473cf90dd7addd4fb7f6cb485950bb6c
Katarzyna-W <39034151+Katarzyna-W@users.noreply.github.com>
2025-07-08 12:48:19 +0200

    Update generateSequence.hpp

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    if (step == 0 or count < 1) {
+    if (count < 1) {
         return {};
     }
     std::vector<int> resultVec{};
