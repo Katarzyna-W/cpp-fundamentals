9165e1113142915c4fff8e4650f498c452a068dd
Katarzyna-W <39034151+Katarzyna-W@users.noreply.github.com>
2025-07-09 11:49:29 +0200

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include <string>
 
-enum class ErrorCode {Ok = 2, 
-                    PasswordNeedsAtLeastNineCharacters = 3,
-                    PasswordNeedsAtLeastOneNumber = 4, 
-                    PasswordNeedsAtLeastOneSpecialCharacter = 5,
-                    PasswordNeedsAtLeastOneUppercaseLetter = 6,
-                    PasswordsDoNotMatch = 7};
+enum class ErrorCode { Ok = 2, 
+                       PasswordNeedsAtLeastNineCharacters = 3,
+                       PasswordNeedsAtLeastOneNumber = 4, 
+                       PasswordNeedsAtLeastOneSpecialCharacter = 5,
+                       PasswordNeedsAtLeastOneUppercaseLetter = 6,
+                       PasswordsDoNotMatch = 7 };
                     
 std::string getErrorMessage(ErrorCode code);
 bool doPasswordsMatch(std::string p1, std::string p2);
