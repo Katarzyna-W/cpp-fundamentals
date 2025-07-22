941c19c7159ae556f7df56da31b4cdf4dcf6bb23
Katarzyna-W <39034151+Katarzyna-W@users.noreply.github.com>
2025-07-22 16:01:26 +0200

    Update vectorFunctions.cpp

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,16 +1,16 @@
+#include "vectorFunctions.hpp"
 #include <iostream>
 #include <string>
-#include "vectorFunctions.hpp"
 
 std::vector<std::shared_ptr<int>> generate(int count) {
-    std::vector<std::shared_ptr<int>> vec {};
-    for (size_t i = 0; i < count; i++){
+    std::vector<std::shared_ptr<int>> vec{};
+    for (size_t i = 0; i < count; i++) {
         std::shared_ptr<int> sp1 = std::make_shared<int>(i);
         vec.push_back(sp1);
     }
     return vec;
-  
 }
+
 void print(std::vector<std::shared_ptr<int>> vec) {
     for (std::shared_ptr<int> elem : vec) {
         std::cout << *elem << "\n";
@@ -18,21 +18,23 @@ void print(std::vector<std::shared_ptr<int>> vec) {
 }
 
 void add10(std::vector<std::shared_ptr<int>> vec) {
-    if (vec) {
-        for (std::shared_ptr<int> elem : vec) {
+    for (std::shared_ptr<int> elem : vec) {
+        if (elem){
             *elem += 10;
         }
     }
 }
+
 void sub10(int *ptr) {
     if (ptr) {
         *ptr -= 10;
     }
 }
+
 void sub10(std::vector<std::shared_ptr<int>> vec) {
-    if (vec) {
-        for (auto elem : vec) {
-            sub10(elem.get());
+    for (auto elem : vec) {
+        if (elem) {
+                    sub10(elem.get());
         }
     }
 }
