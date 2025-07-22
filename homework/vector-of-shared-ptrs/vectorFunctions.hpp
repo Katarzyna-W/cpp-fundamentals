6c96284e4536c2750533d98ea2d895a63c228929
Katarzyna-W <39034151+Katarzyna-W@users.noreply.github.com>
2025-07-22 15:32:55 +0200

    Create vectorFunctions.hpp

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.hpp
@@ -0,0 +1,7 @@
+#include <vector>
+
+std::vector<std::shared_ptr<int>> generate(int count);
+void print(std::vector<std::shared_ptr<int>>);
+void add10(std::vector<std::shared_ptr<int>>);
+void sub10(int *ptr);
+void sub10(std::vector<std::shared_ptr<int>>);
