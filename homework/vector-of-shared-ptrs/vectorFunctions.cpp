7e1f34d44a0c8b5dd041205fb00a4a413de9755a
Katarzyna-W <39034151+Katarzyna-W@users.noreply.github.com>
2025-07-22 16:05:01 +0200

    Update vectorFunctions.cpp

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -19,13 +19,13 @@ void print(std::vector<std::shared_ptr<int>> vec) {
 
 void add10(std::vector<std::shared_ptr<int>> vec) {
     for (std::shared_ptr<int> elem : vec) {
-        if (elem){
+        if (elem) {
             *elem += 10;
         }
     }
 }
 
-void sub10(int *ptr) {
+void sub10(int* ptr) {
     if (ptr) {
         *ptr -= 10;
     }
@@ -34,7 +34,7 @@ void sub10(int *ptr) {
 void sub10(std::vector<std::shared_ptr<int>> vec) {
     for (auto elem : vec) {
         if (elem) {
-                    sub10(elem.get());
+            sub10(elem.get());
         }
     }
 }
