b28ccc7f4a0aba8c74dde7a87ac5954462dbf512
Katarzyna-W <39034151+Katarzyna-W@users.noreply.github.com>
2025-07-22 15:51:20 +0200

    Create vectorFunctions.cpp

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include "vectorFunctions.hpp"
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+    std::vector<std::shared_ptr<int>> vec {};
+    for (size_t i = 0; i < count; i++){
+        std::shared_ptr<int> sp1 = std::make_shared<int>(i);
+        vec.push_back(sp1);
+    }
+    return vec;
+}
+
+void print(std::vector<std::shared_ptr<int>> vec) {
+    for (std::shared_ptr<int> elem : vec) {
+        std::cout << *elem << "\n";
+    }
+}
+
+void add10(std::vector<std::shared_ptr<int>> vec) {
+        for (std::shared_ptr<int> elem : vec) {
+            *elem += 10;
+    }
+}
+
+void sub10(int *ptr) {
+    *ptr -= 10;
+}
+
+void sub10(std::vector<std::shared_ptr<int>> vec) {
+    for (auto elem : vec) {
+        sub10(elem.get());
+    }
+}
